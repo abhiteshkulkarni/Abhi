3de730dc43d1e7615c5f3171b9b5d939622dc3f4
Abhi <116165162+abhiteshkulkarni@users.noreply.github.com>
2023-11-28 18:57:12 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main{
+  public static void main(String args[])
+  {
+    System.out.println("Hello world,My Name is Abhitesh KUlkarni")
+    System.out.println("Hello world,My Name is Ashitosh Jagtap")
+  }
+}
